b603447b3fb4af56977bb9144dfbc9e04b01b3db
Caleb Owens <caleb.ja.owens@gmail.com>
2020-07-11 22:17:41 -0400

    InventoryBomb v1.0 : - Plugin is finished

---
+++ src/main/java/net/dohaw/play/inventorybomb/InventoryBomb.java
@@ -0,0 +1,59 @@
+package net.dohaw.play.inventorybomb;
+
+import me.c10coding.coreapi.APIHook;
+import net.dohaw.play.inventorybomb.commands.InventoryBombCommand;
+import org.bukkit.Bukkit;
+
+import java.io.File;
+
+/*
+    Plugin: InventoryBomb
+    For: mithzan
+    Description: A plugin that drops all the person's items (minus equipped things) on the floor around a player at a certain radius
+ */
+public final class InventoryBomb extends APIHook {
+
+    private final String PREFIX = "[&bInventoryBomb&r]";
+
+    @Override
+    public void onEnable() {
+
+        hookAPI(this);
+        getLogger().info("CoreAPI Hooked!");
+
+        validateFiles();
+        registerCommands();
+    }
+
+    @Override
+    public void onDisable() { }
+
+    private void registerCommands(){
+        Bukkit.getServer().getPluginCommand("inventorybomb").setExecutor(new InventoryBombCommand(this));
+        getLogger().info("Registered commands...");
+    }
+
+    private void validateFiles(){
+        File[] files = new File[]{new File(getDataFolder(), "config.yml")};
+        for(File f : files){
+            if(!f.exists()){
+                saveResource(f.getName(), false);
+            }
+        }
+        getLogger().info("Validated files!");
+    }
+
+    public int getRadius(){
+        if(getConfig().get("Radius") != null){
+            if(getAPI().getMathHelper().isInt(getConfig().getString("Radius"))){
+                return getConfig().getInt("Radius");
+            }
+        }
+        return 3;
+    }
+
+    public String getPrefix(){
+        return PREFIX;
+    }
+
+}
---
+++ src/main/java/net/dohaw/play/inventorybomb/commands/InventoryBombCommand.java
@@ -0,0 +1,117 @@
+package net.dohaw.play.inventorybomb.commands;
+
+import me.c10coding.coreapi.chat.ChatFactory;
+import me.c10coding.coreapi.helpers.MathHelper;
+import net.dohaw.play.inventorybomb.InventoryBomb;
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.scoreboard.Team;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+/*
+    Plugin: InventoryBomb
+    For: mithzan
+    Description: A plugin that drops all the person's items (minus equipped things) on the floor around a player at a certain radius
+ */
+public class InventoryBombCommand implements CommandExecutor {
+
+    private InventoryBomb plugin;
+    private ChatFactory chatFactory;
+    private MathHelper mathHelper;
+    private final String PREFIX;
+    private int radius;
+
+    public InventoryBombCommand(InventoryBomb plugin){
+        this.plugin = plugin;
+        this.chatFactory = plugin.getAPI().getChatFactory();
+        this.PREFIX = plugin.getPrefix();
+        this.mathHelper = plugin.getAPI().getMathHelper();
+        this.radius = plugin.getRadius();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(args[0].equalsIgnoreCase("t") || args[0].equalsIgnoreCase("p")){
+            if(args.length == 2){
+                String playerName = args[1];
+                if(args[0].equalsIgnoreCase("p")){
+                    if(Bukkit.getPlayer(playerName) != null){
+                        Player player = Bukkit.getPlayer(playerName);
+                        bombInventory(player);
+                    }else{
+                        chatFactory.sendPlayerMessage("This is not a valid player!", true, sender, PREFIX);
+                    }
+                }else if(args[0].equalsIgnoreCase("t")){
+                    String teamName = args[1];
+                    if(Bukkit.getScoreboardManager().getMainScoreboard() != null){
+                        if(Bukkit.getScoreboardManager().getMainScoreboard().getTeam(teamName) != null){
+                            Team team = Bukkit.getScoreboardManager().getMainScoreboard().getTeam(teamName);
+                            bombInventory(team);
+                        }else{
+                            chatFactory.sendPlayerMessage("This is not a valid team!", true, sender, PREFIX);
+                        }
+                    }
+                }
+            }
+        }else if(args[0].equalsIgnoreCase("reload")){
+            plugin.reloadConfig();
+            this.radius = plugin.getRadius();
+            chatFactory.sendPlayerMessage("&aReloaded the config!", true, sender, PREFIX);
+        }
+        return false;
+    }
+
+    private void bombInventory(Player player){
+
+        PlayerInventory playerInventory = player.getInventory();
+        List<ItemStack> invItems = Arrays.asList(playerInventory.getStorageContents());
+        final Location playerLocation = player.getLocation();
+
+        for(int x = 0; x < invItems.size(); x++){
+            if(playerInventory.getItem(x) != null){
+
+                ItemStack currentItem = invItems.get(x);
+                playerInventory.setItem(x, null);
+
+                int randXAlter = mathHelper.getRandomInteger(radius, radius * -1);
+                int randZAlter = mathHelper.getRandomInteger(radius, radius * -1);
+                Location itemDropLocation = new Location(player.getWorld(), playerLocation.getX() + randXAlter, playerLocation.getY() + 2, playerLocation.getZ() + randZAlter);
+
+                itemDropLocation.getWorld().dropItem(itemDropLocation, currentItem);
+
+            }
+        }
+
+        player.getWorld().spawnParticle(Particle.END_ROD, playerLocation, 50);
+        player.getWorld().playSound(playerLocation, Sound.ENTITY_GENERIC_EXPLODE, 1, 1);
+
+    }
+
+    private void bombInventory(Team team){
+
+        Set<OfflinePlayer> setTeamPlayers = team.getPlayers();
+        List<OfflinePlayer> listOfflinePlayers = new ArrayList<>(setTeamPlayers);
+        List<Player> listOnlinePlayers = new ArrayList<>();
+
+        for(OfflinePlayer op : listOfflinePlayers){
+            if(op.isOnline()){
+                listOnlinePlayers.add(op.getPlayer());
+            }
+        }
+
+        int randomIndex = mathHelper.getRandomInteger(listOnlinePlayers.size() - 1, 0);
+        Player randomPlayer = listOnlinePlayers.get(randomIndex);
+        bombInventory(randomPlayer);
+    }
+
+
+}
